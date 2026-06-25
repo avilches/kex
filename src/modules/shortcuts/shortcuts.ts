3608a5866212620bfe4211594eb328f146dd5393
Alberto Vilches <github@betauer.aleeas.com>
2026-06-24 22:11:29 -0400

    feat(split-view): add previewMode string union type and toggleSplit shortcut

--- src/modules/shortcuts/shortcuts.ts
+++ src/modules/shortcuts/shortcuts.ts
@@ -58,6 +58,7 @@ export type ShortcutId =
   | "editor.markdown.toggleView"
   | "editor.save"
   | "editor.html.toggleView"
+  | "editor.preview.toggleSplit"
   | "path.copy"
   | "notifications.jumpToLast";
 
@@ -431,6 +432,12 @@ export const SHORTCUTS: Shortcut[] = [
     label: "Toggle HTML preview",
     group: "Editor",
     defaultBindings: [{ [MOD_PROP]: true, shift: true, key: "v" }],
+  },
+  {
+    id: "editor.preview.toggleSplit",
+    label: "Toggle split preview",
+    group: "Editor",
+    defaultBindings: [],
   },
   {
     id: "editor.undo",
